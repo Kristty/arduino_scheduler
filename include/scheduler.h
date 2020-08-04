afac5fbf7c54f937ce6ca43b3f295b75ac4d9d6a
Kristina Dmitrieva <kristin1kozlovskaya@gmail.com>
2020-08-04 23:18:33 +0200

    Implement scheduler

--- include/scheduler.h
+++ include/scheduler.h
@@ -5,4 +5,47 @@
 #ifndef SCHEDULER_H
 #define SCHEDULER_H
 
+#define DEFAULT_LOCATION "Europe/Amsterdam"
+
+#include <Arduino.h>
+#include <ezTime.h>
+
+typedef struct {
+  uint8_t sec;
+  uint8_t min;
+  uint8_t hr;
+  uint8_t day;
+  uint8_t mnth;
+  uint16_t year;
+  uint8_t intervalDays;
+} schedule;
+
+class Scheduler {
+public:
+  void (*function)();
+  uint8_t startSec, startMin, startHr, startDay, startMnth;
+  uint16_t startYear;
+  uint8_t intervalDays;
+
+  Scheduler();
+
+  void init(void (*function)(), schedule, String* = new String(DEFAULT_LOCATION));
+
+  time_t getStartUnixTime();
+  time_t getNextUnixTime();
+
+  /*
+   * Print Date/Time in a customizable format
+   * Available formats are listed here: https://github.com/ropg/ezTime#datetime
+   */
+  String getStartDateTime(const String format = DEFAULT_TIMEFORMAT);
+  String getNextDateTime(const String format = DEFAULT_TIMEFORMAT);
+
+  void initTimezone(String*);
+  void setNextEvent();
+
+private:
+  Timezone *_tz;
+};
+
 #endif
--- src/scheduler.cpp
+++ src/scheduler.cpp
@@ -3,3 +3,65 @@
  */
 
 #include "scheduler.h"
+
+Scheduler::Scheduler() {
+};
+
+void Scheduler::init(void (*function)(), schedule schedule, String *location) {
+  this->function = function;
+
+  startSec = schedule.sec;
+  startMin = schedule.min;
+  startHr = schedule.hr;
+  startDay = schedule.day;
+  startMnth = schedule.mnth;
+  startYear = schedule.year;
+  intervalDays = schedule.intervalDays;
+
+  this->_tz = new Timezone();
+  initTimezone(location);
+  setNextEvent();
+}
+
+time_t Scheduler::getStartUnixTime() {
+  return makeTime(startHr, startMin, startSec, startDay, startMnth, startYear);
+}
+
+time_t Scheduler::getNextUnixTime() {
+
+  time_t t = getStartUnixTime();
+  time_t intervalDaySecs = intervalDays * SECS_PER_DAY;
+
+  while (t <= _tz->now())
+  {
+    t += intervalDaySecs;
+  }
+
+  return t;
+};
+
+String Scheduler::getStartDateTime(const String format) {
+  return _tz->dateTime(getStartUnixTime(), format);
+}
+
+String Scheduler::getNextDateTime(const String format) {
+  return _tz->dateTime(getNextUnixTime(), format);
+}
+
+void Scheduler::initTimezone(String *location) {
+  // ezTime logging level, one of: NONE, ERROR, INFO, DEBUG
+  ezt::setDebug(NONE);
+  ezt::waitForSync();
+
+  if (!_tz->setCache(0) && _tz->getOlson() != *location) {
+    Serial.println(F("Timezone info not found in cache, fetching it"));
+    _tz->setLocation(*location);
+  }
+  
+  // TODO extract method for current time logging and remove from scheduler init
+	Serial.println(*location + " time: " + _tz->dateTime());
+}
+
+void Scheduler::setNextEvent() {
+    _tz->setEvent(function, getNextUnixTime());
+}
